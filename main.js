602d6ac91e02402fd9feeabbb5c0a467d25fb999
Ritika Tyagi <142375391+Ritika-tyagii@users.noreply.github.com>
2024-08-08 21:08:46 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+
+let mic = document.getElementById("mic");
+let chatareamain = document.querySelector('.chatarea-main');
+let chatareaouter = document.querySelector('.chatarea-outer');
+
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+const recognition = new SpeechRecognition();
+
+function showusermsg(usermsg){
+    let output = '';
+    output += <div class="chatarea-inner user">${usermsg}</div>;
+    chatareaouter.innerHTML += output;
+    return chatareaouter;
+}
+recognition.onresult=function(e){
+    let resultIndex = e.resultIndex;
+    let transcript = e.results[resultIndex][0].transcript;
+    chatareamain.appendChild(showusermsg(transcript));
+  
+   console.log(transcript);
+}
+
+mic.addEventListener("click", function(){
+    recognition.start();
+    console.log("Activated");
+})
+
+
+
+
